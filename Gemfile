eb74c0202bd16a850cef5ebf82cb713ea9226ed6
Zachary Weiss <coolshale@gmail.com>
2017-03-13 15:17:24 -0400

    basic seeds work

--- Gemfile
+++ Gemfile
@@ -7,6 +7,10 @@ end
 
 gem 'bcrypt',         '3.1.11'
 
+gem 'json'
+
+gem 'activerecord-import'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def burn_down_the_house
+	Course.destroy_all
+	Instructor.destroy_all
+	Subject.destroy_all
+end
+
+def seed_courses
+	json = JSON.parse(File.read('db/seeds/course.json'))
+
+	json.each do |course|
+		Course.create(name: course["name"], independent_study: course["independent_study"], code: course["code"], description: course["description"])
+	end
+
+end
+
+def seed_instructors
+	json = JSON.parse(File.read('db/seeds/instructor.json'))
+
+	json.each do |instructor|
+		Instructor.create(first: instructor["first"], middle: instructor["middle"], last: instructor["last"], email: instructor["email"])
+	end
+end
+
+def seed_subjects
+	json = JSON.parse(File.read('db/seeds/subject.json'))
+
+	json.each do |a|
+		Subject.create(name: a["name"])
+	end
+
+end
+
+burn_down_the_house
+seed_courses
+seed_instructors
+seed_subjects
