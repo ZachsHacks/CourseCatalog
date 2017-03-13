f81da4ebcb09cfd545ed605dfe7e09533af8403e
Zachary Weiss <coolshale@gmail.com>
2017-03-13 14:22:18 -0400

    made routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+
+	get 'courses/' , to: "courses#index"
+
+  get 'subjects/' , to: "subjects#index"
+
+  get 'instructors/' , to: "instructors#index"
+
+
   resources :users
+
 	root "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313172349) do
+ActiveRecord::Schema.define(version: 20170313180853) do
+
+  create_table "courses", force: :cascade do |t|
+    t.boolean  "independent_study"
+    t.string   "name"
+    t.string   "code"
+    t.integer  "credits"
+    t.text     "description"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  create_table "instructors", force: :cascade do |t|
+    t.string   "first"
+    t.string   "middle"
+    t.string   "last"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
