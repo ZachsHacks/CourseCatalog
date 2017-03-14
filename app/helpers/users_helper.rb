6f7f095e9a86bc55dc20cef0f726ff15403b78ad
Zachary Weiss <coolshale@gmail.com>
2017-03-13 21:50:41 -0400

    done

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+	def pretty_print(user)
+		string = ""
+		string << "<h1>Welcome, #{user.first}!</h1>"
+		string << "<h2>email: #{user.email}</h2>"
+		string << "<h3>(use navigation bar to view courses, subjects, and instructors)</h3>"
+		string.html_safe
+	end
+
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,7 +1,11 @@
 class Instructor < ApplicationRecord
 
 	def username
-		username = self.email.to_s.split("@")
-		username[0]
+		if self.email == nil
+			"N/A"
+		else
+			username = self.email.to_s.split("@")
+			username[0]
+		end
 	end
 end
