fce6ce9a2a7bde592ba1ac7243d497be9e7a780c
Zachary Weiss <coolshale@gmail.com>
2017-03-20 19:37:54 -0400

    even better search but not functional

--- db/migrate/20170313180853_create_subjects.rb
+++ db/migrate/20170313180853_create_subjects.rb
@@ -1,8 +1,10 @@
 class CreateSubjects < ActiveRecord::Migration[5.0]
-  def change
-    create_table :subjects do |t|
-      t.string :name
-      t.timestamps
-    end
-  end
+	def change
+		create_table :subjects do |t|
+			t.string :name
+			t.timestamps
+		end
+		add_column :subjects, :subject_id, :string
+	end
+
 end
